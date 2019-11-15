7e91e28df2c0a7c6e256f34bb16b03db1b2ff332
Hyan18 <Hyan18@github.com>
2019-11-15 10:50:38 +0000

    debugged exercise_3

--- exercise_3.rb
+++ exercise_3.rb
@@ -2,10 +2,10 @@ def factorial(n)
   product = 1
   p "at the start product is #{product}"
   while n > 0
-    n -= 1
     p "we multiply #{product} by #{n}"
     product *= n
     p "we get #{product}"
+    n -= 1
   end
   product
 end
